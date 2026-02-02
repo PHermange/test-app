39bd3b5905c260f97b15a10f475fbc07a260fa85
PHermange <pierrehermange.github@gmail.com>
2026-02-02 20:24:35 +0100

    force-dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import prisma from "@/lib/prisma";
 
